726ffba7f3b2790b66482f5519e63a981771f05d
Danae Rozendaal <danaerozendaal@gmail.com>
2015-06-03 12:47:35 -0600

    Adjusted mortality model.

--- Initial model fits mortality.R
+++ Initial model fits mortality.R
@@ -27,12 +27,6 @@ mdata$log.dbh0<-log(mdata$dbh0/20)
 #Put values for all subplots into a list
 split.mdata <- split(mdata,mdata$subplotID)
 
-fDBH<-split(mdata$dbh0,mdata$subplotID)
-fWD<-split(mdata$WD,mdata$subplotID)
-PlotCode<-split(mdata$PlotCode,mdata$subplotID)
-dbhgrowth<-split(mdata$dbhgrowth,mdata$subplotID)
-log.fDBH<-split(mdata$log.dbh0,mdata$subplotID)
-
 ##########################################################################################
 ##########################################################################################
 ##INITIAL MORTALITY MODEL
@@ -43,46 +37,63 @@ comp.fun <- function(target,c2) {
   
   if (!is.finite(min(exp(c2*target)))) print(range(target))
   
-  #return((target/20)^c2)
   return(exp(c2 * target))
   
 }
 
-subplot.comp <- function(dbhs,c0,c1,c2) {
+subplot.comp <- function(subplot,c0_minWD,c0_maxWD,c1_minWD,c1_maxWD,c2_minWD,c2_maxWD) {
+  
+  c0_slope <- (c0_maxWD - c0_minWD)/(max.WD - min.WD)
+  c0_int <- c0_minWD - (c0_slope * min.WD)
+  
+  c1_slope <- (c1_maxWD - c1_minWD)/(max.WD - min.WD)
+  c1_int <- c1_minWD - (c1_slope * min.WD) 
+  
+  c2_slope <- (c2_maxWD - c2_minWD)/(max.WD - min.WD)
+  c2_int <- c2_minWD - (c2_slope * min.WD)
   
   #competition effect matrix
-  comp <- outer(dbhs,rep(c2,length(dbhs)),FUN=comp.fun)
+  c2_WD <- c2_int+c2_slope*subplot$WD  
+  comp <- outer(c2_WD,subplot$log.dbh0,FUN=comp.fun)
+  
+  diag(comp) <- 0
+  #print(comp)
   
   #growth effect on focal trees
-  fcomp <- exp(-c0 * ((exp(dbhs))^c1) * rowSums(comp))   #log dbh to avoid exponent in comp.fun
+  c0_WD <- c0_int + c0_slope*subplot$WD
+  c1_WD <- c1_int + c1_slope*subplot$WD
+  fcomp <- exp(-(c0_WD) * (exp(subplot$log.dbh0*c1_WD)) * rowSums(comp))   #log dbh to avoid exponent in comp.fun
   
 }
 
 #function for predicted mortality rates
-pred.mort<-function(m0,m1,s1,s2,c0,c1,c2,E){
-#pred.mort<-function(m0,m1,s1,s2,c0,c1,c2){
+pred.mort<-function(pl_minWD,pl_maxWD,s1_minWD,s1_maxWD,s2_minWD,s2_maxWD,c0_minWD,c0_maxWD,c1_minWD,c1_maxWD,c2_minWD,c2_maxWD,E){
   
   #Potential longevity
-  WD_slope <- (m1 - m0)/(max.WD - min.WD)
-  WD_int <- m0 - (WD_slope * min.WD)
+  pl_slope <- (pl_maxWD - pl_minWD)/(max.WD - min.WD)
+  pl_int <- pl_minWD - (pl_slope * min.WD)
   
-  pot.longevity <- WD_int + (WD_slope * mdata$WD)
+  pot.longevity <- pl_int + (pl_slope * mdata$WD)
+  #pot.longevity <- 1
   
   #Size effect
-  #s2 <- log(99)/(s4 * (1-s3))
-  #m.size <- ((mdata$dbh0/10)^s1)/(1 + exp(s2 * (mdata$dbh0 - s3 * s4)))
-  m.size <- ((mdata$dbh0/202)^s1) * exp(-s2 * mdata$dbh0)
+  s1_slope <- (s1_maxWD - s1_minWD)/(max.WD - min.WD)
+  s1_int <- s1_minWD - (s1_slope * min.WD)
+  s2_slope <- (s2_maxWD - s2_minWD)/(max.WD - min.WD)
+  s2_int <- s2_minWD - (s2_slope * min.WD)
+  
+  s1_WD <- s1_int + (s1_slope * mdata$WD)
+  s2_WD <- s2_int + (s2_slope * mdata$WD)
+  m.size = ((mdata$dbh0/202)^s1_WD) * exp(-(s2_WD) * mdata$dbh0)
   #m.size <- 1
   
   #Competition effect
-  #m.comp <- c1 + (1 - c1) * exp(-c2 * mdata$subplotBA.m2ha)
+  m.comp = unlist(lapply(split.mdata,FUN=subplot.comp,c0_minWD,c0_maxWD,c1_minWD,c1_maxWD,c2_minWD,c2_maxWD))
   #m.comp <- 1
-  m.comp = unlist(lapply(log.fDBH,FUN=subplot.comp,c0,c1,c2))
   
   #k <- pot.mort * m.size * m.comp
   #p.ann <- 1 / (1+exp(-k))
   p.ann <- 1 / (1 + (pot.longevity * m.size * m.comp * E)) 
-  #p.ann <- 1 / (1 + (pot.longevity * m.size * m.comp)) 
   p.int <- 1 - ((1-p.ann)^mdata$IntervalLength)
   
   p.int[p.int<0.0001] <- 0.0001
@@ -91,12 +102,10 @@ pred.mort<-function(m0,m1,s1,s2,c0,c1,c2,E){
   return(p.int)
 }
 
-mort.ll<-function(m0,m1,s1,s2,c0,c1,c2,E_all,E_mean,E_sd){
-#mort.ll<-function(m0,m1,s1,s2,c0,c1,c2){
+mort.ll<-function(pl_minWD,pl_maxWD,s1_minWD,s1_maxWD,s2_minWD,s2_maxWD,c0_minWD,c0_maxWD,c1_minWD,c1_maxWD,c2_minWD,c2_maxWD,E_all,E_mean,E_sd){
   
   #probability of mortality
-  p.int<-pred.mort(m0,m1,s1,s2,c0,c1,c2,E_all[mdata$PlotCode])
-  #p.int<-pred.mort(m0,m1,s1,s2,c0,c1,c2)
+  p.int<-pred.mort(pl_minWD,pl_maxWD,s1_minWD,s1_maxWD,s2_minWD,s2_maxWD,c0_minWD,c0_maxWD,c1_minWD,c1_maxWD,c2_minWD,c2_maxWD,E_all[mdata$PlotCode])
   
   #assign status (dead/alive) and calculate likelihood
   ll <- log(ifelse(mdata$dead==1,p.int,1-p.int))
@@ -116,19 +125,18 @@ mort.ll<-function(m0,m1,s1,s2,c0,c1,c2,E_all,E_mean,E_sd){
 }
 
 fb.pars.m <- list(
-  m0 = c(1e-3,1000,1,1,0,1),
-  m1 = c(1e-3,1000,1,1,0,1),
-  #s1_0 = c(1e-6,3,1,0,1,1),  
-  #s1_1 = c(1e-6,3,1,0,1,1), 
-  #s2_0 = c(1e-6,10,1,0,1,1),
-  #s2_1 = c(1e-6,10,1,0,1,1),
-  s1 = c(1e-6,3,1,0,0,1),
-  s2 = c(1e-6,10,1,0,0,1),
-  c0 = c(-50,50,1,0,0,1),      
-  c1 = c(-5,5,0,0,0,1),
-  c2 = c(-5,5,2,0,0,1),
-  #c2_int = c(-5,5,2,0,0,1),
-  #c2_slope = c(-5,5,2,0,0,1),
+  pl_minWD = c(1e-3,1000,1,1,0,1),
+  pl_maxWD = c(1e-3,1000,1,1,0,1),
+  s1_minWD = c(1e-6,10,1,0,0,1),  
+  s1_maxWD = c(1e-6,10,1,0,0,1), 
+  s2_minWD = c(1e-6,10,1,0,0,1),
+  s2_maxWD = c(1e-6,10,1,0,0,1),
+  c0_minWD = c(-10,10,1,0,0,1),
+  c0_maxWD = c(-10,10,1,0,0,1),
+  c1_minWD = c(-5,5,0,0,0,1),
+  c1_maxWD = c(-5,5,0,0,0,1),
+  c2_minWD = c(-5,5,2,0,0,1),
+  c2_maxWD = c(-5,5,2,0,0,1),
   E_all = c(1e-3,1,1,0,1,0,181),
   E_mean = c(1e-3,1,1,1,1,1),
   E_sd = c(1e-3,1,1,1,1,1)
@@ -138,21 +146,14 @@ fb.out.m<-filzbach(40000,20000,mort.ll,nrow(mdata),fb.pars.m)
 df.fb.out<-as.data.frame(fb.out.m)
 write.table(df.fb.out,"FB output mort model 1.txt",row.names=F,quote=F,sep="\t")
 
-final_out <- paste(readLines("C:/Users/rozendad/Dropbox/Current projects/UofR/ForestDynamics model runs/workspace/Default_MCMC_final_out.txt"), collapse="\t")
+final_out <- paste(readLines("C:/Users/DMAR/Dropbox/Current projects/UofR/ForestDynamics model runs/workspace/Default_MCMC_final_out.txt"), collapse="\t")
 write.table(final_out,"Final_out mort model 1.txt",row.names=F,quote=F,sep="\t")
 
-#Convergence (converged)
+#Convergence
 pdf("Model mort 1.pdf",width=8,height=4)
-par(mfrow=c(1,2),mar=c(5,4,1,1))
 mort.llvec<-function(x) mort.ll(x[1],x[2],x[3],x[4],x[5],x[6],x[7])
 fb.out.ll<-apply(fb.out.m,1,mort.llvec)
 plot(fb.out.ll,type="l",main="40000/20000")
-
-#Calculate goodness of fit: how to calculate?? Per plot/subplot?
-fb.pm<-colMeans(fb.out.m)
-pred<-pred.mort(fb.pm[1],fb.pm[2],fb.pm[3],fb.pm[4],fb.pm[5],fb.pm[6],fb.pm[7])
-plot(pred,mdata$dead,main=paste("r2=",cor(pred,mdata$dead)^2))
-abline(0,1)
 
 dev.off()
 
@@ -160,56 +161,3 @@ dev.off()
 fb.ci<-apply(fb.out.m,2,FUN=quantile,probs=c(0.025,0.5,0.975))
 df.fb.ci<-as.data.frame(fb.ci)
 write.table(df.fb.ci,"Parameters mort model 1.txt",row.names=F,quote=F,sep="\t")
-
-#  s1 = c(1e-3,10,1,1,1,1),
-#  s3 = c(1e-3,100,1,1,1,1),
-#  s4 = c(1e-3,1,1,0,1,1),
-
-##########################################################################################
-##########################################################################################
-##########################################################################################
-##########################################################################################
-##########################################################################################
-##########################################################################################
-##RECRUITMENT
-
-#Number of stems/ha/yr per subplot
-#Potential rate * landscape (plot) effect. A measure of WD? Spp not possible
-
-library(filzbach)
-
-#Calculate recruitment rate
-rdata<-aggregate(data$recruit,list(data$PlotCode,data$subplotID,data$subplot.area,
-                                   data$subplot.no.trees,data$region,data$IntervalLength,
-                                   data$CensusDate,data$subplotBA.m2ha),sum,na.rm=T)
-names(rdata)<-c("PlotCode","subplotID","subplot.area","subplot.no.trees",
-                "region","IntervalLength","CensusDate","subplotBA.m2ha","sum.recruits")
-rdata$recr.rate<-rdata$sum.recruits/rdata$IntervalLength
-
-meanWD<-aggregate(data$WD,list(data$PlotCode),mean,na.rm=T)
-names(meanWD)<-c("PlotCode","meanWD")
-
-rdata2<-merge(rdata,meanWD,all.x=T)
-
-hist(rdata2$recr.rate,breaks=100)
-
-#function for predicted recruitment rates
-pred.recr<-function(r1,w1){
-  
-  #potential recruitment could be a function of WD: no (aggregated over stems...)
-  pot.recr = r1
-  
-  #landscape WD, ranging from 0 to 1, or absolute cwm?
-  #Add still
-  r.LWD = min(1,(w1 + (1-w1)))
-  
-  #subplot BA?
-  #Add? Same structure as for WD, but probably not a large effect for 10 cm trees
-  #Although it may work through in growth.
-  
-  pred = pot.recr * r.LWD
-  
-  return(pred)
-}
-
-#likelihood: GAMMA OR ZERO-INFLATED GAMMA?
